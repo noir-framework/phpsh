cb33084da06b5cbc39c208217bb263af7497d791
int986@gmail.com <int986@gmail.com>
2023-08-04 16:01:59 +0300

    add more tests

--- tests/ScriptingTest.php
+++ tests/ScriptingTest.php
@@ -10,7 +10,8 @@ class ScriptingTest extends TestCase
 {
 
     /** @test */
-    public function nextLine(): void {
+    public function createNextLine(): void
+    {
 
         $script = (new Script())
             ->echo('test')
@@ -25,6 +26,98 @@ class ScriptingTest extends TestCase
             ->generate();
 
         $this->assertEquals("echo -n test \\\\\n", $script);
+
+    }
+
+    /** @test */
+    public function createKillCommand()
+    {
+
+        $script = (new Script())
+            ->kill(1, 15)
+            ->generate();
+
+        $this->assertEquals('kill -15 1', $script);
+
+        $command = (new Script())
+            ->command('kill', [-15, 1])
+            ->generate();
+
+        $this->assertEquals($command, $script);
+
+    }
+
+    /** @test */
+    public function createSemiColon()
+    {
+
+        $script = (new Script())
+            ->echo('test')
+            ->semiColon()
+            ->generate();
+
+        $this->assertEquals('echo -n test;', $script);
+
+        // Execute it!
+        $this->assertEquals('test', shell_exec($script));
+
+        $command = (new Script())
+            ->command('echo', ['-n', 'test'])
+            ->put(';')
+            ->generate();
+
+        $this->assertEquals($command, $script);
+
+        $command = (new Script())
+            ->echo('test')
+            ->put(';')
+            ->generate();
+
+        $this->assertEquals($command, $script);
+
+    }
+
+    /** @test */
+    public function createTouchCommand(): void
+    {
+
+        $script = (new Script())
+            ->touch('test.txt')
+            ->generate();
+
+        $this->assertEquals('touch test.txt', $script);
+
+        $command = (new Script())
+            ->command('touch', ['test.txt'])
+            ->generate();
+
+        $this->assertEquals($command, $script);
+
+    }
+
+    /** @test */
+    public function createExitCommand()
+    {
+
+        $script = (new Script())
+            ->exit(10)
+            ->generate();
+
+        $this->assertEquals('exit 10', $script);
+
+        $script = (new Script())
+            ->exit(10)
+            ->semiColon()
+            ->generate();
+
+        $this->assertEquals('exit 10;', $script);
+
+        $command = (new Script())
+            ->command('exit', [10])
+            ->put(';')
+            ->generate();
+
+        $this->assertEquals($command, $script);
 
     }
 
